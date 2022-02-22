a0188122ccacbbadb4ccee6c4eb33c346f4e7e9b
Azokah <jaimegbonorino@gmail.com>
2022-02-22 10:02:16 -0300

    Update files structure

--- DMTools/dmt_app/views/home_view.py
+++ DMTools/dmt_app/views/home_view.py
@@ -1,8 +1,13 @@
 from django.http import HttpResponse
+from django.template import loader
 from ..services import firebase_service as firebase_service
 
 def home_view(request):
-    user_data = firebase_service.FirebaseService.get_user_data("email", "password")
+    user_data = firebase_service.FirebaseService.get_user_data("jaimegbonorino@gmail.com", "123321")
     request.session["user_token"] = user_data["token"]
     email = user_data["email"]
-    return HttpResponse(f"Hello, {email}. You're at the DM-Tools home page.")
+    template = loader.get_template("home.html")
+    context = {
+        "greeting": f"Hi, {email}",
+    }
+    return HttpResponse(template.render(context, request))
